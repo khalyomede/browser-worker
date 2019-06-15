5357b51851415f5bf52e515cec7f1df180a3a3d7
Khalyomede <khalyomede@gmail.com>
2019-06-15 13:59:16 +0200

    added mutation test

---
+++ stryker.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+	config.set({
+		mutator: "javascript",
+		packageManager: "yarn",
+		reporters: ["clear-text", "progress", "dashboard"],
+		testRunner: "mocha",
+		transpilers: ["babel"],
+		testFramework: "mocha",
+		coverageAnalysis: "off",
+		babel: {
+			optionsFile: ".babelrc"
+		},
+		files: ["lib/**/*.js", "test/**/*.js"]
+	});
+};
