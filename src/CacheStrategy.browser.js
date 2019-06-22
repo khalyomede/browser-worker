fe1ab47d2422cd46b231687f426091ab05c25dcc
Khalyomede <khalyomede@gmail.com>
2019-06-22 14:37:12 +0200

    removed useless files for the browser side scripts

--- src/CacheStrategy.browser.js
+++
@@ -1,5 +0,0 @@
-import { CacheStrategy } from "./main";
-
-if (!("CacheStrategy" in window)) {
-	window.CacheStrategy = CacheStrategy;
-}
--- src/Route.browser.js
+++
@@ -1,5 +0,0 @@
-import { Route } from "./main";
-
-if (!("Route" in window)) {
-	window.Route = Route;
-}
